096ac2beb8c074eac13c9058bfaed1f5c59a6762
Abdimatin <abdimatAbdullahi@gmail.com>
2023-12-13 11:11:03 +0300

    Validation Form

--- src/components/FormFix.tsx
+++ src/components/FormFix.tsx
@@ -4,16 +4,39 @@ import {DevTool} from '@hookform/devtools';
 type FormValues = {
     email: string,
     password: string
+    username: string,
+    social:{
+        twitter: "",
+        facebook: ""
+    }
+    phoneNumbers: string[]
 }
 
+
 const FormFix = () => {
-    const form = useForm<FormValues>();
-    const {register, control, handleSubmit, formState} = form;
+    const form = useForm<FormValues>({
+        defaultValues: {
+            email: "abdimatin@infosend.com",
+            social: {
+                twitter: "",
+                facebook: ""
+            },
+            phoneNumbers: ["", ""],
+            username: ""
+        }
+      });
+
+    const {register, control, handleSubmit, formState, getValues} = form;
     const {errors} = formState; 
 
     const onSubmit = (data: FormValues)=>{
         console.log('Form Values', data);
     }
+
+    const getValueHandler = ()=>{
+        console.log('Form Values', getValues());
+    }
+
 
   return (
     <div>
@@ -34,13 +57,60 @@ const FormFix = () => {
             <p className='error'>{errors.email?.message}</p>
 
             <label htmlFor="password">Password</label>
-            <input type="password" id='password' {...register("password",{
-                required: "Password Field Required"
-            })} />
+            <input type="password" id='password' {...register("password",{required: "Password Field Required"})} />
             <p className='error'>{errors.password?.message}</p>
 
+            <label htmlFor="twitter">Twitter</label>
+            <input type="text" id='twitter' {...register("social.twitter",{required: "Twitter Profile Required"})} />
+            <p className='error'>{errors.social?.twitter?.message}</p>
+
+            <label htmlFor="facebook">Facebook</label>
+            <input type="text" id='facebook' {...register("social.facebook", {required: "Fcaebook Profile Required"})} />
+            <p className='error'>{errors.social?.facebook?.message}</p>
+
+            {/* <label htmlFor="primary">Primary Phone Number</label>
+
+            <input type="text" id='primary' {...register("phoneNumbers.0", {
+                pattern:{
+                    value: /^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/,
+                    message: "Enter Phone Number Starting With Country Code"
+                },
+                validate: (fieldValue)=>{
+                    return(
+                        fieldValue.startsWith("+") || 
+                        "Start With country Code"
+                    )
+                }
+            })} />
+            <p className='error'>{errors.phoneNumbers?.[1]?.message}</p> */}
+
+            {/* <label htmlFor="seconadry">Secondary Phone Number</label>
+            <input type="text" id='secondary' {...register("phoneNumbers.1",  {
+                pattern:{
+                    value: /^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/,
+                    message: "Enter Phone Number Starting With Country Code"
+                },
+                validate: (fieldValue)=>{
+                    return(
+                        fieldValue.startsWith("+") || 
+                        "Start With country Code"
+                    )
+                }
+            })} />
+            <p className='error'>{errors.phoneNumbers?.[0]?.message}</p> */}
+
+            <label htmlFor="username">UserName</label>
+            <input type="text" id='username' {...register("username", {
+                validate: (value)=>{
+                    return (
+                        value.startsWith("@") || 'Username Should Start With One'
+                    )
+                }
+            })} />
+            <p className='error' >{errors.username?.message}</p>
 
             <button>Submit</button>
+            <button type='button' onClick={getValueHandler} >Get Form Values</button>
         </form>
         <DevTool control={control}/>
     </div>
